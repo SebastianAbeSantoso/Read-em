91aac136863d24a3801b017d06eec627aff28c12
Ashmslmhh <ashmasalimah@gmail.com>
2026-06-16 10:17:56 +0800

    finishing

--- app/src/main/java/com/example/proyek_akhir_kewirausahaan/MainActivity.kt
+++ app/src/main/java/com/example/proyek_akhir_kewirausahaan/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.compose.runtime.SideEffect
 import com.example.proyek_akhir_kewirausahaan.ui.theme.ProyekAkhirKewirausahaanTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val view = LocalView.current
+            SideEffect {
+                val window = (view.context as android.app.Activity).window
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+            }
             ProyekAkhirKewirausahaanTheme {
                 ReademApp()
             }
